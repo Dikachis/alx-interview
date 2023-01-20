4ad7aef265793181560e40a0b8e0a18d921902d7
Dikachis <dikachi015safety@gmail.com>
2023-01-20 02:09:46 +0100

    Added some files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,54 @@
 #!/usr/bin/python3
-"""Log Parser"""
 
-from sys import stdin
-
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
-tot_size = 0
-count = 0
+import sys
 
 
-def printStatistics():
-    print('File size: {}'.format(tot_size))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
-    for line in stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in status.keys():
-                status[code] += 1
-            tot_size += size
-            count += 1
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-        if count == 10:
-            count = 0
-            printStatistics()
+        if len(parsed_line) > 2:
+            counter += 1
 
-except Exception as err:
-    pass
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
 finally:
-    printStatistics()
+    print_msg(dict_sc, total_file_size)
